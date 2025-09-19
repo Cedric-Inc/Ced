dc3f84b3dbd61fcc6dfeca1eb231a6f2eb9b860e
Cedric_ <506437665cx@gmail.com>
2025-09-19 23:21:02 +0800

    Update train.py
    20G  setup

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ training_args = TrainingArguments(
     output_dir="./outputs",
     overwrite_output_dir=True,
     num_train_epochs=2,
-    per_device_train_batch_size=128,
+    per_device_train_batch_size=64,
     gradient_accumulation_steps=2,
     learning_rate=2e-4,
     warmup_steps=500,
@@ -92,3 +92,4 @@ login("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")
 
 model.push_to_hub("UserCedric/Ced-33M", overwrite=True)
 tokenizer.push_to_hub("UserCedric/Ced-33M", overwrite=True)
+
